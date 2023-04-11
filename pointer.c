302f4b23d4d1ba2ababe0fbe60af185f76f92f92
Utsab Sarkar <107045746+utsabsarkar12@users.noreply.github.com>
2023-04-11 19:39:27 +0600

    Add files via upload

---
+++ pointer.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int n=5,m=10;
+    int *n1,*m1;
+    n1=&n;
+    m1=&m;
+    //scanf("%d",&person1.age);
+
+    printf("%d , %d\n",&n,n1);
+
+    return 0;
+}
+
