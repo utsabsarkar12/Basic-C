ce6ad4f670d72857c8a3c286977283544f534619
Utsab Sarkar <107045746+utsabsarkar12@users.noreply.github.com>
2023-04-15 00:08:53 +0600

    Add files via upload

---
+++ Piramid Star.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()  {
+    int i, j, rows=4, k = 0;
+
+
+    for ( i =1; i <= rows; i++)  {
+        for ( j = 1; j <= rows - i; j++) {
+            printf ("  ");
+        }
+        for ( k = 1; k <= ( 2 * i - 1); k++){
+            printf ("* ");
+        }
+        printf ("\n");
+    }
+    return 0;
+}
